2c4c34dd0109cd1465f00dd1fc4db776c2467a65
Imron-DE <imronrosyadi97@gmail.com>
2024-12-06 23:56:59 +0700

    saya menambahkan tugas 5

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,7 +1,12 @@
-// import styles from "./styles.module.css";
-
 const Footer = () => {
-  return <div className="text-3xl font-bold underline">Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">&copy; {new Date().getFullYear()} Footer. All rights reserved.</p>
+        <p className="text-xs mt-2">Copyright by Imron</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,14 +1,25 @@
-import React from "react";
-import Menu from "../Menu.js";
-import { WithAuth } from "../With-auth/index.js";
-import styles from "./styles.module.css";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <Menu />
-    </div>
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl font-bold">Header</div>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/" className="text-white hover:text-gray-200">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/profile" className="text-white hover:text-gray-200">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
-export default WithAuth(Header);
+export default Header;
