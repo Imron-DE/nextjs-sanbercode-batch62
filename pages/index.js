213dc04c81766124b656eef268b22b40f68a1e9d
Imron-DE <imronrosyadi97@gmail.com>
2024-12-09 14:42:39 +0700

    saya menaambahkan tugas 6

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 export default function Home() {
   return (
     <>
-      <Layout>
+      <Layout metaTitle="Home" metaDescription="ini adalah halaman home">
         <p>Home</p>
       </Layout>
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -3,7 +3,7 @@ import Layout from "./Layout";
 export default function Home() {
   return (
     <>
-      <Layout>
+      <Layout metaTitle="Profile - Imron Rosadi" metaDescription="Halaman profil Imron Rosadi. Lihat informasi tentang saya, pengalaman, dan proyek yang saya kerjakan di bidang pengembangan web.">
         <p>Profile</p>
       </Layout>
     </>
