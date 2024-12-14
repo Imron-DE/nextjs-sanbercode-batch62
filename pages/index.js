fd4b54945403492fd7953dcfab3a7e37182a37d3
Imron-DE <imronrosyadi97@gmail.com>
2024-12-14 08:03:24 +0700

    Saya menambahkan tugas 10

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
-import Layout from "./Layout";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const Layout = dynamic(() => import("./Layout"), { ssr: false });
 
 export default function Home() {
   useEffect(() => {
@@ -13,6 +16,7 @@ export default function Home() {
     <>
       <Layout metaTitle="Home" metaDescription="ini adalah halaman home">
         <p>Home</p>
+        <Image src="/NextJs.jpg" alt="Next Img" width={400} height={400} />
       </Layout>
     </>
   );
--- pages/users/index.js
+++ pages/users/index.js
@@ -4,7 +4,7 @@ export default function users() {
   return (
     <>
       <Layout>
-        <p>Users</p>
+        <p>users</p>
       </Layout>
     </>
   );
